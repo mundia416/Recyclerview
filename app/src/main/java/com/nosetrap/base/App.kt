13422941e1797cad99ca2ea8b9a356940c853723
Mundia <fxmundia416@gmail.com>
2019-05-23 14:51:40 +0200

    initial commit

---
+++ app/src/main/java/com/nosetrap/base/App.kt
@@ -0,0 +1,16 @@
+package com.nosetrap.base
+
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //setup leak canary
+        if (BuildConfig.DEBUG) {
+            LeakCanary.install(this)
+        }
+    }
+}
