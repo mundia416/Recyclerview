468dd38a10eb4b76c85728f61f08c58b973586eb
Mundia <fxmundia416@gmail.com>
2019-05-24 22:23:38 +0200

    updated dependencies

--- app/src/main/java/com/nosetrap/base/activity/MainActivity.kt
+++ app/src/main/java/com/nosetrap/base/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.nosetrap.base
+package com.nosetrap.base.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nosetrap.base.R
 
 class MainActivity : AppCompatActivity() {
 
